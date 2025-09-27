cbc2332aa0fd0f5a523b85a976e785ab602f3273
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-09-27 15:03:56 -0400

    feat: add session start/stop and loading actiong types + state shape

--- src/store/peer/peerTypes.ts
+++ src/store/peer/peerTypes.ts
@@ -1,11 +1,13 @@
 export enum PeerActionType {
-    PEER_SESSION_START = 'PEER_SESSION_START',
-    PEER_SESSION_STOP = 'PEER_SESSION_STOP',
-    PEER_LOADING = 'PEER_LOADING'
+	PEER_SESSION_START = 'PEER_SESSION_START',
+	PEER_SESSION_STOP = 'PEER_SESSION_STOP',
+	PEER_LOADING = 'PEER_LOADING',
 }
 
 export interface PeerState {
-    readonly id?: string
-    readonly loading: boolean
-    readonly started: boolean
+	readonly id?: string
+	readonly loading: boolean
+	readonly started: boolean
 }
+
+
