937121fa8c47e69dc3d1b3064a0999c552f55017
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-09-27 14:58:59 -0400

    feat: ass use aysnc state with promise setter for sequences udate

--- src/helpers/hooks.ts
+++ src/helpers/hooks.ts
@@ -1,11 +1,13 @@
-import {useState} from "react";
+import { useState } from 'react'
 
 export const useAsyncState = (initialValue: any) => {
-    const [value, setValue] = useState(initialValue);
-    const setter = (x: any) =>
-        new Promise<void>(resolve => {
-            setValue(x);
-            resolve();
-        });
-    return [value, setter];
+	const [value, setValue] = useState(initialValue)
+	const setter = (x: any) =>
+		new Promise<void>((resolve) => {
+			setValue(x)
+			resolve()
+		})
+	return [value, setter] as const
 }
+
+
