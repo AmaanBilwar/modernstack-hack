037f865dadf0bfaeabc7635028b0d51eaf413627
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-09-27 20:16:31 -0400

    basic reformatting

--- src/store/peer/peerReducer.ts
+++ src/store/peer/peerReducer.ts
@@ -1,27 +1,28 @@
-import type { Reducer } from 'redux'
-import { PeerActionType, type PeerState } from './peerTypes'
+import type { Reducer } from "redux";
+import { PeerActionType, type PeerState } from "./peerTypes";
 
 export const initialState: PeerState = {
-	id: undefined,
-	loading: false,
-	started: false,
-}
+  id: undefined,
+  loading: false,
+  started: false,
+};
 
-export const PeerReducer: Reducer<PeerState> = (state = initialState, action) => {
-	switch (action.type) {
-		case PeerActionType.PEER_SESSION_START: {
-			const { id } = action as any
-			return { ...state, id, started: true }
-		}
-		case PeerActionType.PEER_SESSION_STOP:
-			return { ...initialState }
-		case PeerActionType.PEER_LOADING: {
-			const { loading } = action as any
-			return { ...state, loading }
-		}
-		default:
-			return state
-	}
-}
-
-
+export const PeerReducer: Reducer<PeerState> = (
+  state = initialState,
+  action,
+) => {
+  switch (action.type) {
+    case PeerActionType.PEER_SESSION_START: {
+      const { id } = action as any;
+      return { ...state, id, started: true };
+    }
+    case PeerActionType.PEER_SESSION_STOP:
+      return { ...initialState };
+    case PeerActionType.PEER_LOADING: {
+      const { loading } = action as any;
+      return { ...state, loading };
+    }
+    default:
+      return state;
+  }
+};
